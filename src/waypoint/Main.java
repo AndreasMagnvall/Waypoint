5a5b9542ea75322f934896bd84abf79877a0b45c
Froadus <simjoh99@gmail.com>
2017-02-22 22:57:19 +0100

    Skapa projektet

---
+++ src/waypoint/Main.java
@@ -0,0 +1,40 @@
+package waypoint;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.*;
+import java.text.MessageFormat;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main extends JavaPlugin {
+
+	private static Main instance;
+
+	public static List<ProtectedArea> area;
+	public static long saveTime = System.currentTimeMillis();
+
+	public static Map<String, InProgressArea> inProgressAreas = new HashMap<>();
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public void onEnable(){
+
+	}
+
+	@Override
+	public void onDisable() {
+
+	}
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("newcuboid") && sender instanceof Player) {
+
+		}
+		return false;
+	}
+}
